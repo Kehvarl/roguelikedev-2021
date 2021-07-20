02e3e963c70e44c036c08210b51d6353a5459aa5
Kehvarl <kehvarl@kehvarl.com>
2021-07-19 17:39:22 -0700

    Prepping for A* pathfinding

---
+++ pathfinding.lisp
@@ -0,0 +1,25 @@
+(in-package #:roguelike-2021)
+
+(defparameter *all-directions*
+  (list (cons 0 -1)
+        (cons 0 1)
+        (cons -1 0)
+        (cons 1 0)
+        (cons -1 -1)
+        (cons -1 1)
+        (cons 1 -1)
+        (cons 1 1)))
+
+(defclass node ()
+  ((g :initform 0 :accessor node/g)
+   (h :initform 0 :accessor node/h)
+   (f :initform 0 :accessor node/f)
+   (distance-from-parent :initarg :distance-from-pareng :accessor
+                         node/distance-from-parent)
+   (parent :initarg :parent :initform nil :accessor node/parent)
+   (position :initarg :position :initform nil :accessor node/position)))
+
+(defmethod print-object ((obj node) stream)
+  (print-unreadable-object (obj stream :type t)
+    (with-slots (position parent) obj
+      (format stream "~A, parent ~A" position parent))))
