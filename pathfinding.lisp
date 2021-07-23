47ee62f0c2864887a5d9e64f6c1954a39180a329
Kehvarl <kehvarl@kehvarl.com>
2021-07-23 08:44:06 -0700

    last necessary component for AStar

--- pathfinding.lisp
+++ pathfinding.lisp
@@ -80,3 +80,23 @@ CHILD-NODE, then it pushes CHILD-NODE onto OPEN-LIST"
                             child-node))
       (t
        (queues:qpush open-list child-node)))))
+
+(defun generate-node-children (current-node map open-list closed-list end-node)
+  "Gemerate a list of all valid nodes that can be moves to from CURRENT-NODE.
+and adds them to OPEN-QUEUE.  A valid node is within the MAP dimensions, the
+tile is not blocking, and the node is not in the CLOSED-LIST"
+  (dolist (new-position *all-directions*)
+    (let ((node-x (+ (car (node/position current-node))
+                     (car new-position)))
+          (node-y (+ (cdr (node/position current-node))
+                     (cdr new-position))))
+      (unless (or (> node-x (1- (game-map/w map)))
+                  (< node-x 0)
+                  (> node-y (1- (game-map/h map)))
+                  (< node-y 0))
+        (unless (tile/blocked (aref (game-map/tiles map) node-x node-y))
+          (let ((child (make-node current-node node-x node-y new-position)))
+            ;; Child is on the closed list.
+            (unless (find child closed-list :test 'node-equal)
+              (generate-node-cost child current-node end-node)
+              (update-open-queue open-list child))))))))
