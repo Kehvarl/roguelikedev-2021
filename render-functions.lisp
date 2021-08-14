e3ded3691de4219f1b0d3e9089c3a27725bec711
Kehvarl <kehvarl@kehvarl.com>
2021-08-13 19:56:29 -0700

    not quite right

--- render-functions.lisp
+++ render-functions.lisp
@@ -15,13 +15,14 @@
   (< (getf *render-order* (entity/render-order entity-1))
      (getf *render-order* (entity/render-order entity-2))))
 
-(defun render-all (game-state player map stats-panel screen-width screen-height
-                              &key (debug_track nil))
+(defun render-all (game-state player map stats-panel screen-width screen-height)
+                              ;&key (debug_track nil))
   (blt:clear)
   (dotimes (y (game-map/h map))
     (dotimes (x (game-map/w map))
       (let* ((tile (aref (game-map/tiles map) x y))
-             (track (tile/track tile))
+             ;(track (tile/track tile))
+             (door (tile/door tile))
              (wall (tile/blocked tile))
              (visible (tile/visible tile))
              (explored (tile/explored tile)))
@@ -37,10 +38,10 @@
                    (setf (blt:background-color) (getf *color-map* :dark-ground)))
                 (setf (blt:cell-char x y) #\Space)))
 
-        (when (and debug_track track)
+        (when door;(and debug_track track)
           (if wall
             (setf (blt:background-color) (getf *color-map* :dark-wall))
-            (setf (blt:background-color) (blt:hsva track 180 180)))
+            (setf (blt:background-color) (blt:white))) ;(blt:hsva track 180 180)))
          (setf (blt:cell-char x y) #\Space)))))
 
   (let ((entities (game-state/entities game-state)))
